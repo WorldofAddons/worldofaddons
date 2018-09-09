b66e5f8b912c838b6c89d7f4e1c01d1af7ac3ec6
zainkai <turkingtonkevin@gmail.com>
2018-09-09 15:03:51 -0700

    WIP: addon table

--- src-react/components/AddonTable.jsx
+++ src-react/components/AddonTable.jsx
@@ -1,5 +1,60 @@
 import React, { Component } from 'react'
+import { ipcRenderer } from 'electron'
 
 export class AddonTable extends Component {
-  
+  constructor(props){
+    super(props)
+
+    this.state = {
+      addonList: []
+    }
+
+    // event listeners
+    ipcRenderer.on('newAddonObj', this.addRow)
+    ipcRenderer.on('updateAddonStatus', (e, updateObj) => {}) // TODO: fix download status
+  }
+
+  addRow(e, newAddon) {
+    let { addonList } = this.state
+    addonList.push(newAddon)
+    this.setState({addonList})
+  }
+
+  onInstall(e) {
+
+  }
+
+  onRemove(e) {
+
+  }
+
+  renderRow(addonObj) {
+    return(
+      <tr>
+        <td>{addonObj.name}</td>
+        <td>{addonObj.host}</td>
+        <td>{addonObj.version}</td>
+        <td>{"TODO: download Status"}</td>
+        <td>
+          <button onClick={this.onInstall.bind(this)}>Install</button>
+        </td>
+        <td>
+          <button onClick={this.onRemove.bind(this)}>Remove</button>
+        </td>
+      </tr>
+    )
+  }
+
+  render(){
+    const tags = this.state.addonList.map(a => this.renderRow(a))
+
+    return(
+      <div>
+        <table>
+          {tags}
+        </table>
+      </div>
+    )
+  }
+
 }
--- src/curseforge.js
+++ src/curseforge.js
@@ -3,7 +3,7 @@ export function parseCurseforgeHTML(pageHTML) {
     const page = parser.parseFromString(pageHTML, "text/html");
     const link = page.getElementsByClassName('download__link')[0].href
     const cleanLink = link.substring(link.lastIndexOf("/wow/") + 1, link.lastIndexOf("/file")) + "/file" // Remove "file///<windows drive whatever>" from the link
-    const downloadUrl =  "https://www.curseforge.com/" + cleanLink
-    
+    const downloadUrl =  `https://www.curseforge.com/${cleanLink}`
+
     return downloadUrl
 }
