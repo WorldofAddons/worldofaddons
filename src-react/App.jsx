8eeef2cb43926269bc3bbcb362f73220c0fabe14
Kevin Turkington <turkingtonkevin@gmail.com>
2018-10-11 19:29:43 -0700

    init react-redux

--- src-react/App.jsx
+++ src-react/App.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Dashboard} from './screens/Dashboard'
-import {Footer} from './Footer'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<Dashboard />, document.getElementById('root'))
+import reducers from './reducers/index'
+import { Dashboard } from './screens/Dashboard'
+import { Footer } from './Footer'
+
+const store = createStore(reducers)
+
+class App extends React.Component {
+  render () {
+    return (
+        <div>
+          <Dashboard />
+        </div>
+    )
+  }
+}
+
+ReactDOM.render(<Provider store={store}><App/></Provider>,document.getElementById('root'))
 ReactDOM.render(<Footer />, document.getElementById('footer'))
