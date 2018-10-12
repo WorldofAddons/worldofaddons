c87e8fd500b54c085f64bf66101fb2a209d82b30
Kevin Turkington <turkingtonkevin@gmail.com>
2018-10-11 18:59:32 -0700

    btn refactor

--- src-react/Footer.jsx
+++ src-react/Footer.jsx
@@ -4,31 +4,38 @@ import { shell } from 'electron'
 const { app } = window.require('electron').remote
 
 export class Footer extends React.PureComponent {
-    openIssues () {
-        // TODO: dangerous?
-        shell.openExternal('https://github.com/WorldofAddons/worldofaddons/issues/new')
-    }
+  openIssues() {
+    // TODO: dangerous?
+    shell.openExternal('https://github.com/WorldofAddons/worldofaddons/issues/new')
+  }
 
-    renderCopyright () {
-        return (
-        <div className="footer-copyright">
-            <div className="container">
-                Version: {app.getVersion()}
-            <a 
-                className="grey-text text-lighten-4 right"
-                onClick={this.openIssues.bind(this)}>
-                Report an Bug
-            </a>
-            </div>
-        </div>
-        )
-    }
+  renderIssuesBtn() {
+    return (
+      <button
+        className='btn-small waves-effect waves-light right'
+        onClick={this.openIssues.bind(this)}
+      >
+        Report a Bug
+        </button>
+    )
+  }
 
-    render () {
-        return (
-        <div className='grey page-footer'>
-            {this.renderCopyright()}
+  renderCopyright() {
+    return (
+      <div className="footer-copyright">
+        <div className="container">
+          Version: {app.getVersion()}
+          {this.renderIssuesBtn()}
         </div>
-        )
-    }
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div className='grey page-footer'>
+        {this.renderCopyright()}
+      </div>
+    )
+  }
 }
