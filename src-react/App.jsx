1abe1acec37401d882436c5b74f07bc4581c7532
Kevin Turkington <turkingtonkevin@gmail.com>
2018-10-15 22:14:39 -0700

    working webpack config

--- src-react/App.jsx
+++ src-react/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Dashboard from './screens/Dashboard'
+import {Dashboard} from './screens/Dashboard'
 
 ReactDOM.render(<Dashboard />, document.getElementById('root'))
