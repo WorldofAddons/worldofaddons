17b3de8cd7046db235d3f7d40c55760dfc4bf2bc
Kevin Turkington <turkingtonkevin@gmail.com>
2018-10-18 18:44:25 -0700

    finished checkWhichHost tests

--- src/checkWhichHost/__tests__/spec.js
+++ src/checkWhichHost/__tests__/spec.js
@@ -5,11 +5,18 @@ describe('checkWhichHost function', () => {
     const addonName = 'dank-memes'
     const host = 'curseforge'
     const validURL = `https://www.${host}.com/wow/addons/${addonName}`
-    it('THEN should init a valid URL object', () => {
+    it('THEN should return a valid URL object', () => {
       const result = checkWhichHost(validURL)
       expect(result.URL).toEqual(validURL)
       expect(result.name).toEqual(addonName)
       expect(result.host).toEqual(host)
+    })
+  })
+  describe('GIVEN a invalid url', () => {
+    const invalidUrl = `https://www.KevinIsTheBest.com/wow/addons/JetFuel`
+    it('THEN should return a error object', () => {
+      const result = checkWhichHost(invalidUrl)
+      expect(result).toHaveProperty('error')
     })
   })
 })
