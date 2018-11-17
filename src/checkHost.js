b8fc257481f014987be69041585fdd4c2d0aae36
khlam <khlam@users.noreply.github.com>
2018-11-16 22:04:58 -0800

    adding support for wowinterface

--- src/checkHost.js
+++ src/checkHost.js
@@ -4,6 +4,10 @@ export function checkWhichHost (url) {
     return initCurseforgeObj(url)
   }
 
+  if (url.startsWith('https://www.wowinterface.com/downloads/')) {
+    return initWoWInterface(url)
+  }
+
   const errorObj = { 'error': `Invalid url '${url}'. Given link does not match parse` }
   return errorObj
 }
@@ -14,5 +18,17 @@ export function checkWhichHost (url) {
 function initCurseforgeObj (url) {
   const URLSplit = url.split('https://www.curseforge.com/wow/addons/')
   const URLObj = { 'url': url, 'host': 'curseforge', 'name': URLSplit[1] }
+  return URLObj
+}
+
+// Creates a JSON object for addons hosted by WoWInterface.
+// Parses the addon name from the end of the url, this name is how the
+// JSON object is referenced by other components
+function initWoWInterface (url) {
+  const URLSplit = url.split('https://www.wowinterface.com/downloads/')
+  const URLObj = { 'url': url, 'host': 'wowinterface', 'name': URLSplit[1].substring(
+    URLSplit[1].lastIndexOf("-") + 1, 
+    URLSplit[1].lastIndexOf(".html")
+)}
   return URLObj
 }
